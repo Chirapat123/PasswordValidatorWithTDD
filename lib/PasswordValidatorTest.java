0f2419b91e6e9fb9401a25d342f3fcf7b1240f99
Chirapat123 <chiraphat.v@ku.th>
2025-07-03 17:37:13 +0700

    Add files via upload

--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -14,18 +14,24 @@ public class PasswordValidatorTest{
         }
 
         // --- เขียน Test Case อื่นๆ ต่อ ---
+
+        // Test Case 2: ตรวจสอบรหัสผ่านที่เป็น ตัวอักษรพิมพ์เล็ก พิมพ์ใหญ่ ตัวเลข สัญญาลักษณ์อื่นๆ 1หรือ2 รูปแบบ WEAK
         PasswordStrength result2 = PasswordValidator.validate("12N");
         if (result2 == PasswordStrength.WEAK) {
             System.out.println("Test Case 2 Passed: Short password is WEAK.");
         } else {
             System.out.println("Test Case 2 FAILED: Expected WEAK but got " + result2);
         }
+
+        // Test Case 3: ตรวจสอบรหัสผ่านที่เป็น ตัวอักษรพิมพ์เล็ก พิมพ์ใหญ่ ตัวเลข สัญญาลักษณ์อื่นๆ 3 รูปแบบ MEDIUM
         PasswordStrength result3 = PasswordValidator.validate("DGHDH8544#FGFa");
         if (result3 == PasswordStrength.MEDIUM) {
             System.out.println("Test Case 3 Passed: Short password is MEDIUM.");
         } else {
             System.out.println("Test Case 3 FAILED: Expected MEDIUM but got " + result3);
         }
+
+        // Test Case 4: ตรวจสอบรหัสผ่านที่เป็น ตัวอักษรพิมพ์เล็ก พิมพ์ใหญ่ ตัวเลข สัญญาลักษณ์อื่นๆ 4 รูปแบบ STRONG
         PasswordStrength result4 = PasswordValidator.validate("AAAAAAAAA44444444444");
         if (result4 == PasswordStrength.STRONG) {
             System.out.println("Test Case 4 Passed: Short password is STRONG.");
